0bbe4e5764c26fdc312dc75c76df4ce78f5c927e
IvanOvcharuk <ivanovcharuk228@gmail.com>
2022-10-21 16:15:44 +0200

    Przypisywanie tekstu do etykietu

--- app/src/main/java/com/example/aplikacjemobilne/MainActivity.kt
+++ app/src/main/java/com/example/aplikacjemobilne/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.aplikacjemobilne
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.but1_tekst).setOnClickListener {
+            var text1 = findViewById<TextView>(R.id.Etykietimieinazwisko).text.toString();
+            var text2 = findViewById<EditText>(R.id.Poletkst).text.toString();
+            findViewById<TextView>(R.id.Etykietimieinazwisko).text = text1 + text2;
+        }
     }
 }
